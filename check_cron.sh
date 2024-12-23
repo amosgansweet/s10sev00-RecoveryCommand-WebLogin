86fff4757bc7d73d5008068fde558a12378c7ee7
amosgansweet <171889997+amosgansweet@users.noreply.github.com>
2024-12-23 18:44:55 +0800

    Update check_cron.sh

--- check_cron.sh
+++ check_cron.sh
@@ -5,12 +5,12 @@ WORKDIR="/home/${USER}/.nezha-agent"
 FILE_PATH="/home/${USER}/.s5"
 CRON_S5="nohup ${FILE_PATH}/s5 -c ${FILE_PATH}/config.json >/dev/null 2>&1 &"
 CRON_NEZHA="nohup ${WORKDIR}/start.sh >/dev/null 2>&1 &"
-HY2="nohup /home/${USER}/hy2/hy2.sh  >/dev/null 2>&1 &"
+SB10="/home/${USER}/restartsb.sh  >/dev/null 2>&1 &"
 
 
 
 
 echo "检查是否运行，如果无，就启动"
- ps aux | grep -v grep | grep server > /dev/null || bash -c "$HY2"
+ ps aux | grep -v grep | grep s10sb > /dev/null || bash -c "$SB10"
  ps aux | grep -v grep | grep s5 > /dev/null || bash -c "$CRON_S5"
  ps aux | grep -v grep | grep nezha-agent > /dev/null || bash -c "$CRON_NEZHA"
