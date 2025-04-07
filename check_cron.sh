df5aca2065a93d21d1aa6f5ba5eb324e94183bb2
amosgansweet <171889997+amosgansweet@users.noreply.github.com>
2025-04-07 15:28:02 +0800

    Update check_cron.sh

--- check_cron.sh
+++ check_cron.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CRON_S5="nohup $HOME/.5sweb/5sweb -c ${FILE_PATH}/config.json >/dev/null 2>&1 &"
+CRON_S5="nohup $HOME/.5sweb/5sweb -c $HOME/.5sweb/config.json >/dev/null 2>&1 &"
 CRON_NEZHA="nohup $HOME/.tneganz/start.sh >/dev/null 2>&1 &"
 SB10="nohup $HOME/restartsb.sh  >/dev/null 2>&1 &"
 
