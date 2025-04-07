efd6249ec20e2e8e4e1931d689bcb1e6e13b95f9
amosgansweet <171889997+amosgansweet@users.noreply.github.com>
2025-04-07 13:11:42 +0800

    Update check_cron.sh

--- check_cron.sh
+++ check_cron.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 USER=$(whoami)
-WORKDIR="/home/${USER}/.nezha-agent"
-FILE_PATH="/home/${USER}/.s5"
-CRON_S5="nohup ${FILE_PATH}/s5 -c ${FILE_PATH}/config.json >/dev/null 2>&1 &"
+WORKDIR="/home/${USER}/.tneganz"
+FILE_PATH="/home/${USER}/.5sweb"
+CRON_S5="nohup ${FILE_PATH}/5sweb -c ${FILE_PATH}/config.json >/dev/null 2>&1 &"
 CRON_NEZHA="nohup ${WORKDIR}/start.sh >/dev/null 2>&1 &"
 SB10="/home/${USER}/restartsb.sh  >/dev/null 2>&1 &"
 
@@ -12,5 +12,5 @@ SB10="/home/${USER}/restartsb.sh  >/dev/null 2>&1 &"
 
 echo "检查是否运行，如果无，就启动"
  ps aux | grep -v grep | grep s10sb > /dev/null || bash -c "$SB10"
- ps aux | grep -v grep | grep s5 > /dev/null || bash -c "$CRON_S5"
- ps aux | grep -v grep | grep nezha-agent > /dev/null || bash -c "$CRON_NEZHA"
+ ps aux | grep -v grep | grep 5sweb > /dev/null || bash -c "$CRON_S5"
+ ps aux | grep -v grep | grep tneganz > /dev/null || bash -c "$CRON_NEZHA"
