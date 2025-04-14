3cb8967ebe4188ad2a85d80cd4f2323f5c2dc1e5
amosgansweet <171889997+amosgansweet@users.noreply.github.com>
2025-04-14 11:09:43 +0800

    Update check_cron.sh

--- check_cron.sh
+++ check_cron.sh
@@ -2,10 +2,10 @@
 
 #CRON_S5="nohup $HOME/.5sweb/5sweb -c $HOME/.5sweb/config.json >/dev/null 2>&1 &"
 #CRON_NEZHA="nohup $HOME/.tneganz/start.sh >/dev/null 2>&1 &"
-SB10="nohup $HOME/restartsb.sh  >/dev/null 2>&1 &
+SB10="nohup $HOME/checkweb.sh  >/dev/null 2>&1 &
 
 
 echo "检查是否运行，如果无，就启动"
- ps aux | grep -v grep | grep s10sb > /dev/null || bash -c "$SB10"
+ ps aux | grep -v grep | grep webh2 > /dev/null || bash -c "$SB10"
  #ps aux | grep -v grep | grep 5sweb > /dev/null || bash -c "$CRON_S5"
  #ps aux | grep -v grep | grep tneganz > /dev/null || bash -c "$CRON_NEZHA"
